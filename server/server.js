8ac263780b8ff9b318c4757597023d3c579eaee3
Estefania Sosa <estefi.monumental@gmail.com>
2022-08-16 15:00:11 -0400

    polished server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const cors = require('cors')
 
 const path = require('path')
 
-const app = express()
+app.use(cors())
+app.use(express.json())
+// const app = express()
 
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../index.html'));

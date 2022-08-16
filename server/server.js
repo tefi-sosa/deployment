a4d486f1c1c13e932c7b96476cde9f510e9e0a66
Estefania Sosa <estefi.monumental@gmail.com>
2022-08-16 15:42:24 -0400

    Fixed path on package.json

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const app = express()
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
+console.log(path.join(__dirname, '../client/index.html'))
 
 const port = process.env.PORT || 4005
 // This will get the PORT variable from Heroku. However, if one isn't assigned (ex. when we are testing locally) it will use port 4005.
